c7aa246e634baac4f8b39c0c8165142f08890ab3
Adrian Olczyk <adrian@olczyk.me>
2015-08-14 13:46:37 +0200

    Add user's profile information and bump the lib version

---
+++ test/profile.js
@@ -0,0 +1,30 @@
+
+var chai   = require('chai'),
+    assert = chai.assert,
+    SMSAPI = require(__dirname + '/../lib/smsapi.js'),
+    config = require('./config.js');
+
+describe('profile', function(){
+    var smsapi = new SMSAPI({ server: config.server });
+
+    before(function(done){
+        smsapi.authentication.login(config.username, config.password)
+            .then(done.bind(null, null))
+            .catch(done);
+    });
+
+    it('should get profile data', function(done){
+        smsapi.profile.get()
+            .execute()
+            .then(function(result){
+                assert.property(result, 'name');
+                assert.property(result, 'username');
+                assert.property(result, 'email');
+                assert.equal(result.username, config.username);
+                done();
+            })
+            .catch(function(err){
+                console.log('err:', err);
+            });
+    });
+});
