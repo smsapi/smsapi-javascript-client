6a18f294e08b5423a3b7ca09322fff81602e8ec1
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-07-21 13:52:31 +0200

    add typings export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 import { SMSAPIcom } from './smsapi/com';
 import { SMSAPIpl } from './smsapi/pl';
-export { SMSAPIcom, SMSAPIpl };
+import * as SMSAPI from './types/index';
+export { SMSAPIcom, SMSAPIpl, SMSAPI };
