7cfab06315c8f94d0ad48e1ec142b6b9c4c30f91
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-07-31 11:56:33 +0200

    types refactor

--- src/modules/templates/index.ts
+++ src/modules/templates/index.ts
@@ -6,28 +6,35 @@ import { Template } from './types/Template';
 
 export class Templates extends BaseModule {
   async get(): Promise<ApiCollection<Template>> {
-    return await this.httpClient.get('/sms/templates');
+    return await this.httpClient.get<any, ApiCollection<Template>>(
+      '/sms/templates'
+    );
   }
 
   async getById(templateId: string): Promise<Template> {
-    return await this.httpClient.get(`/sms/templates/${templateId}`);
+    return await this.httpClient.get<any, Template>(
+      `/sms/templates/${templateId}`
+    );
   }
 
   async create(newTemplate: NewTemplate): Promise<Template> {
-    return await this.httpClient.post('/sms/templates', newTemplate);
+    return await this.httpClient.post<any, Template>(
+      '/sms/templates',
+      newTemplate
+    );
   }
 
   async update(
     templateId: string,
     newTemplate: Partial<NewTemplate>
   ): Promise<Template> {
-    return await this.httpClient.put(
+    return await this.httpClient.put<any, Template>(
       `/sms/templates/${templateId}`,
       newTemplate
     );
   }
 
   async remove(templateId: string): Promise<void> {
-    await this.httpClient.delete(`/sms/templates/${templateId}`);
+    await this.httpClient.delete<any, void>(`/sms/templates/${templateId}`);
   }
 }
