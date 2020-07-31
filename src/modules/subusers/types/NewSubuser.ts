539c9a25950e741bdb52a14b12c1b9c48ebe4550
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-07-31 17:31:09 +0200

    task-7314 - add subusers support

---
+++ src/modules/subusers/types/NewSubuser.ts
@@ -0,0 +1,9 @@
+import { SubuserCredentials } from './SubuserCredentials';
+import { SubuserPoints } from './SubuserPoints';
+
+export interface NewSubuser {
+  credentials: SubuserCredentials;
+  active?: boolean;
+  description?: string;
+  points?: SubuserPoints;
+}
---
+++ src/modules/subusers/types/Subuser.ts
@@ -0,0 +1,9 @@
+import { SubuserPoints } from './SubuserPoints';
+
+export interface Subuser {
+  id: string;
+  username: string;
+  active: boolean;
+  description: string | null;
+  points: SubuserPoints;
+}
