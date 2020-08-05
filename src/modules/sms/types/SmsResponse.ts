d06e7cd1fcbaf050f7d9bb4e51c548a3982abc51
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-08-05 09:48:25 +0200

    add sms support

---
+++ src/modules/sms/types/SmsResponse.ts
@@ -0,0 +1,19 @@
+import { SmsStatus } from './SmsStatus';
+
+export interface SmsResponse {
+  count: number;
+  list: {
+    id: string;
+    points: number;
+    number: string;
+    dateSent: Date;
+    submittedNumber: string;
+    status: SmsStatus;
+    idx: string | null;
+    parts: number;
+    smsText: string;
+  }[];
+  message: string;
+  length: number;
+  parts: number;
+}
