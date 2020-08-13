37a0cc5557068e5b21f3e49d8c3becee307daa20
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-08-13 08:40:19 +0200

    hlr fix

--- src/modules/hlr/index.ts
+++ src/modules/hlr/index.ts
@@ -1,20 +1,29 @@
+import isArray from 'lodash/isArray';
+
 import { BaseModule } from '../baseModule';
 
 import { HlrCheckResponse } from './types/HlrCheckResponse';
 
 export class Hlr extends BaseModule {
   async check(
-    numbers: string[],
-    idx?: string
+    numbers: string | string[],
+    idx?: string | string[]
   ): Promise<HlrCheckResponse | HlrCheckResponse[]> {
+    const params: Record<string, unknown> = {
+      number: isArray(numbers) ? numbers.join(',') : numbers,
+    };
+
+    if (idx) {
+      params.idx = isArray(idx) ? idx.join(',') : idx;
+    }
+
     return await this.httpClient.get<
       HlrCheckResponse | HlrCheckResponse[],
       HlrCheckResponse | HlrCheckResponse[]
     >('/hlr.do', {
       params: {
         format: 'json',
-        idx,
-        number: numbers.join(','),
+        ...params,
       },
     });
   }
