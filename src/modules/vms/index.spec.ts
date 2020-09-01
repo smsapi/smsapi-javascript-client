042fcf1e6a5206bc0f883d6905340e2354e8f050
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-09-01 15:57:02 +0200

    vms test group

--- src/modules/vms/index.spec.ts
+++ src/modules/vms/index.spec.ts
@@ -1,3 +1,5 @@
+import nock from 'nock';
+
 import { SMSAPI } from '../../smsapi';
 
 const {
@@ -231,5 +233,238 @@ describe('Vms', () => {
         'Please add VMS_REMOTE_FILE_PATH env to test vms with remote path'
       );
     }
+  });
+
+  describe('Group vms', () => {
+    afterEach(() => {
+      nock.cleanAll();
+    });
+
+    describe('tts', () => {
+      it('should send vms to group', async () => {
+        // given
+        const groupName = 'someGroupName';
+        const tts = 'Some tts';
+
+        const req = nock(`${SMSAPI_API_URL}`)
+          .post('/vms.do', {
+            details: true,
+            encoding: 'utf-8',
+            format: 'json',
+            group: groupName,
+            tts,
+            tts_lector: 'ewa',
+          })
+          .reply(200, {
+            count: 1,
+            list: [
+              {
+                date_sent: 1598964973,
+                error: null,
+                id: 'someId',
+                idx: null,
+                number: 'someNumber',
+                points: 0.16,
+                status: 'QUEUE',
+                submitted_number: 'someNumber',
+              },
+            ],
+          });
+
+        // when
+        const response = await smsapi.vms.sendVmsToGroup(groupName, tts);
+
+        // then
+        expect(req.isDone()).toBeTruthy();
+        expect(response).toMatchObject({
+          count: 1,
+          list: [
+            {
+              dateSent: expect.any(Date),
+              error: null,
+              id: 'someId',
+              idx: null,
+              number: 'someNumber',
+              points: 0.16,
+              status: 'QUEUE',
+              submittedNumber: 'someNumber',
+            },
+          ],
+        });
+      });
+
+      it('should send vms to many groups', async () => {
+        // given
+        const groupNames = ['someGroupName1', 'someGroupName2'];
+        const tts = 'Some tts';
+
+        const req = nock(`${SMSAPI_API_URL}`)
+          .post('/vms.do', {
+            details: true,
+            encoding: 'utf-8',
+            format: 'json',
+            group: groupNames.join(','),
+            tts,
+            tts_lector: 'ewa',
+          })
+          .reply(200, {
+            count: 1,
+            list: [
+              {
+                date_sent: 1598964973,
+                error: null,
+                id: 'someId',
+                idx: null,
+                number: 'someNumber',
+                points: 0.16,
+                status: 'QUEUE',
+                submitted_number: 'someNumber',
+              },
+            ],
+          });
+
+        // when
+        const response = await smsapi.vms.sendVmsToGroup(groupNames, tts);
+
+        // then
+        expect(req.isDone()).toBeTruthy();
+        expect(response).toMatchObject({
+          count: 1,
+          list: [
+            {
+              dateSent: expect.any(Date),
+              error: null,
+              id: 'someId',
+              idx: null,
+              number: 'someNumber',
+              points: 0.16,
+              status: 'QUEUE',
+              submittedNumber: 'someNumber',
+            },
+          ],
+        });
+      });
+    });
+
+    // TODO: task-7308
+    xdescribe('local file', () => {
+      it('should send vms to group', () => {
+        return;
+      });
+
+      it('should send vms to many groups', () => {
+        return;
+      });
+    });
+
+    describe('remote file', () => {
+      it('should send vms to group', async () => {
+        // given
+        const groupName = 'someGroupName';
+        const pathToRemoteFile = 'somePath';
+
+        const req = nock(`${SMSAPI_API_URL}`)
+          .post('/vms.do', {
+            details: true,
+            encoding: 'utf-8',
+            file: pathToRemoteFile,
+            format: 'json',
+            group: groupName,
+          })
+          .reply(200, {
+            count: 1,
+            list: [
+              {
+                date_sent: 1598964973,
+                error: null,
+                id: 'someId',
+                idx: null,
+                number: 'someNumber',
+                points: 0.16,
+                status: 'QUEUE',
+                submitted_number: 'someNumber',
+              },
+            ],
+          });
+
+        // when
+        const response = await smsapi.vms.sendVmsWithRemoteFileToGroup(
+          groupName,
+          pathToRemoteFile
+        );
+
+        // then
+        expect(req.isDone()).toBeTruthy();
+        expect(response).toMatchObject({
+          count: 1,
+          list: [
+            {
+              dateSent: expect.any(Date),
+              error: null,
+              id: 'someId',
+              idx: null,
+              number: 'someNumber',
+              points: 0.16,
+              status: 'QUEUE',
+              submittedNumber: 'someNumber',
+            },
+          ],
+        });
+      });
+
+      it('should send vms to many groups', async () => {
+        // given
+        const groupNames = ['someGroupName1', 'someGroupName2'];
+        const pathToRemoteFile = 'somePath';
+
+        const req = nock(`${SMSAPI_API_URL}`)
+          .post('/vms.do', {
+            details: true,
+            encoding: 'utf-8',
+            file: pathToRemoteFile,
+            format: 'json',
+            group: groupNames.join(','),
+          })
+          .reply(200, {
+            count: 1,
+            list: [
+              {
+                date_sent: 1598964973,
+                error: null,
+                id: 'someId',
+                idx: null,
+                number: 'someNumber',
+                points: 0.16,
+                status: 'QUEUE',
+                submitted_number: 'someNumber',
+              },
+            ],
+          });
+
+        // when
+        const response = await smsapi.vms.sendVmsWithRemoteFileToGroup(
+          groupNames,
+          pathToRemoteFile
+        );
+
+        // then
+        expect(req.isDone()).toBeTruthy();
+        expect(response).toMatchObject({
+          count: 1,
+          list: [
+            {
+              dateSent: expect.any(Date),
+              error: null,
+              id: 'someId',
+              idx: null,
+              number: 'someNumber',
+              points: 0.16,
+              status: 'QUEUE',
+              submittedNumber: 'someNumber',
+            },
+          ],
+        });
+      });
+    });
   });
 });
