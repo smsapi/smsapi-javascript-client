8b6654b24f75fa78e4de2776b111fce89c1bd3a0
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-09-02 15:38:10 +0200

    refactor

--- src/modules/contacts/modules/groups/index.ts
+++ src/modules/contacts/modules/groups/index.ts
@@ -5,23 +5,16 @@ import { Group } from './types/Group';
 
 export class Groups extends BaseModule {
   async getById(groupId: string): Promise<Group> {
-    const response = await this.httpClient.get<Group, Group>(
+    return await this.httpClient.get<Group, Group>(
       `/contacts/groups/${groupId}`
     );
-
-    return this.formatResponseDates(response);
   }
 
   async create(name: string, details?: CreateGroupDetails): Promise<Group> {
-    const response = await this.httpClient.post<Group, Group>(
-      '/contacts/groups',
-      {
-        name,
-        ...details,
-      }
-    );
-
-    return this.formatResponseDates(response);
+    return await this.httpClient.post<Group, Group>('/contacts/groups', {
+      name,
+      ...details,
+    });
   }
 
   async remove(groupId: string, deleteContacts = false): Promise<void> {
@@ -32,11 +25,11 @@ export class Groups extends BaseModule {
     });
   }
 
-  private formatResponseDates(group: Group): Group {
-    return {
-      ...group,
-      dateCreated: new Date(group.dateCreated),
-      dateUpdated: new Date(group.dateUpdated),
-    };
-  }
+  // private formatResponseDates(group: Group): Group {
+  //   return {
+  //     ...group,
+  //     dateCreated: new Date(group.dateCreated),
+  //     dateUpdated: new Date(group.dateUpdated),
+  //   };
+  // }
 }
