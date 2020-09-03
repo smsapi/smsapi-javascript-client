2949f763571caef434537c4a3bb796677b28e792
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-09-03 12:17:33 +0200

    add missing get all func

--- src/modules/contacts/modules/groups/index.ts
+++ src/modules/contacts/modules/groups/index.ts
@@ -1,10 +1,18 @@
 import { BaseModule } from '../../../baseModule';
+import { ApiCollection } from '../../../../types/ApiCollection';
 
 import { CreateGroupDetails } from './types/CreateGroupDetails';
 import { Group } from './types/Group';
 import { UpdateGroup } from './types/UpdateGroup';
 
 export class Groups extends BaseModule {
+  async get(): Promise<ApiCollection<Group>> {
+    return await this.httpClient.get<
+      ApiCollection<Group>,
+      ApiCollection<Group>
+    >('/contacts/groups');
+  }
+
   async getById(groupId: string): Promise<Group> {
     return await this.httpClient.get<Group, Group>(
       `/contacts/groups/${groupId}`
