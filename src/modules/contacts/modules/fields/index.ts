413154ced31b6316cc52b602464b200b08608dfc
Przemek Dziewa <p.dziewa@smsapi.pl>
2020-09-09 12:43:03 +0200

    task-7467 - add contact fields support

---
+++ src/modules/contacts/modules/fields/index.ts
@@ -0,0 +1,37 @@
+import { BaseModule } from '../../../baseModule';
+import { ApiCollection } from '../../../../types/ApiCollection';
+
+import { Field } from './types/Field';
+import { FieldType } from './types/FieldType';
+
+export class Fields extends BaseModule {
+  async get(): Promise<ApiCollection<Field>> {
+    return await this.httpClient.get<
+      ApiCollection<Field>,
+      ApiCollection<Field>
+    >('/contacts/fields');
+  }
+
+  async create(
+    fieldName: string,
+    fieldType: FieldType = 'text'
+  ): Promise<Field> {
+    return await this.httpClient.post<Field, Field>('/contacts/fields', {
+      name: fieldName,
+      type: fieldType,
+    });
+  }
+
+  async update(fieldId: string, newName: string): Promise<Field> {
+    return await this.httpClient.put<Field, Field>(
+      `/contacts/fields/${fieldId}`,
+      {
+        name: newName,
+      }
+    );
+  }
+
+  async remove(fieldId: string): Promise<void> {
+    await this.httpClient.delete(`/contacts/fields/${fieldId}`);
+  }
+}
---
+++ src/modules/contacts/modules/fields/types/FieldType.ts
@@ -0,0 +1,1 @@
+export type FieldType = 'text' | 'date' | 'email' | 'phone_number' | 'number';
